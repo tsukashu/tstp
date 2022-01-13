3ba9413ff131844c8fb164174c1397c1a02b1537
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-01-13 18:38:02 +0900

    Add ch6-1,2

---
+++ Part1/ch06/ch6-1.py
@@ -0,0 +1,4 @@
+Camus = 'カミュ'
+print(Camus[0])
+print(Camus[1])
+print(Camus[2])
---
+++ Part1/ch06/ch6-2.py
@@ -0,0 +1,7 @@
+def yesterday():
+    input1 = input('昨日??: ')
+    input2 = input('送った??: ')
+    pf = '私は昨日{}を書いて、{}に送った❗'.format(input1,input2)
+    print(pf)
+
+yesterday()
