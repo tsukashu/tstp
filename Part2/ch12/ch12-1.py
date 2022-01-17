df8dadceaada8c09897af8b63321818f3c8a04a1
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-01-17 19:02:35 +0900

    Fix ch12-1

--- Part2/ch12/ch12-1.py
+++ Part2/ch12/ch12-1.py
@@ -1,14 +1,12 @@
 class Apple:
-    def __init__(self,p,c='red',w=100,t=''):
+    def __init__(self, p, c="red", w=100, t=""):
         self.price = p
         self.color = c
         self.weight = w
         self.taste = t
         print("Apple!!")
 
+
 a1 = apple(100)
 print(a1.color)
 print(a1.price)
-
-
-        
